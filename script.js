8fbbc7065466db2e3cff9dc61ef24cce39bc6e6a
neha-psu <agrawal@pdx.edu>
2019-10-26 10:59:42 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+          
+  $(window).scroll(function(){
+
+    if($(window).scrollTop()>390){
+      $('nav').addClass('color');
+    }else{
+      $('nav').removeClass('color');
+    }
+
+  });
+
+});
+new WOW().init();
